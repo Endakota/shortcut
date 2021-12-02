b985f1721e0c2ca1c9749915d4a307f8bb74ef24
Makhambetali Zhandos <70477573+Endakota@users.noreply.github.com>
2021-12-02 16:46:55 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -2,20 +2,38 @@
 #include <cmath>
 #include <fstream>
 using namespace std;
-bool isRight(double x0, double y0, double x, double y) {
-	if (x0 > 0) return (y0 / x0 * x - y >= 0) ? true : false;
-	else return(y - y0 / x0 * x > 0) ? true : false;
-
-}
-double distance(double x0, double y0, double x, double y) {
-	return (y0 == 0 && x0 != 0) ? fabs(y) : (x0 == 0 && y0 != 0) ? fabs(x) : fabs(y0 / x0 * x - y) / sqrt(y0 * y0 / x0 / x0 + 1);
-}
+class Points {
+	public: 
+		double maxRight[2] = { 0,0 }, maxLeft[2] = {0,0};
+		double x, y;
+		void setXY(double x0, double y0) {
+			this->x = x0;
+			this->y = y0;
+		}
+		bool isRight(double x0, double y0) {
+			if (x0 > 0) return (y0 / x0 * x - y >= 0) ? true : false;
+			else return(y - y0 / x0 * x > 0) ? true : false;
+		}
+		double distance(double x0, double y0){
+			return (x0 == 0 && y0 != 0) ? fabs(x) : fabs(y0 / x0 * x - y) / sqrt(y0 * y0 / x0 / x0 + 1);
+		}
+		void setMaxPoints(double x0, double y0, bool isRight) {
+			if (isRight) {
+				this->maxRight[0] = x0;
+				this->maxRight[1] = y0;
+			}
+			else {
+				this->maxLeft[0] = x0;
+				this->maxLeft[1] = y0;
+			}
+		}
+};
 int main()
 {
 	double x0, y0;
+	Points point;
 	double distance0, distanceR = 0, distanceL = 0;
-	double xrightm = 0, xleftm = 0, yrightm = 0, yleftm = 0;
-	ifstream file("in.txt");
+	ifstream file("C:/Users/Endakota/Documents/Учеба/МатМод/homework/out/build/windows-default/homework1/in.txt");
 	if (!file.is_open()) {
 		cout << "OpenError";
 	}
@@ -23,25 +41,24 @@ int main()
 		file >> x0 >> y0;
 		double x, y;
 		while (file >> x >> y) {
-			distance0 = fabs(distance(x0, y0, x, y));
-			if (isRight(x0, y0, x, y)) {
+			point.setXY(x, y);
+			distance0 = point.distance(x0,y0);
+			if (point.isRight(x0,y0)) {
 				if (distance0 >= distanceR) {
 					distanceR = distance0;
-					xrightm = x;
-					yrightm = y;
+					point.setMaxPoints(x, y, true);
 				}
 			}
 			else {
 				if (distance0 >= distanceL) {
 					distanceL = distance0;
-					xleftm = x;
-					yleftm = y;
+					point.setMaxPoints(x, y, false);
 				}
 			}
 		}
-		cout << "Leftmost: " << xleftm << " " << yleftm << endl;
-		cout << "Rightmost: " << xrightm << " " << yrightm << endl;
+		cout << "Leftmost: " << point.maxLeft[0] << " " << point.maxLeft[1] << endl;
+		cout << "Rightmost: " << point.maxRight[0] << " " << point.maxRight[1] << endl;
 	}
-
+	
 	return 0;
 }
